cdd34373c4b3d48240741fa90f1f0fbe68d37e5f
unknown <sazwerling@gmail.com>
2022-02-11 16:48:01 -0600

    insert documents

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 //CRUD create read update delete
 
-const mongodb = require('mongodb')
+const mongodb = require('mongodb')   //we installed mongodb library. allows connect to mongo database from node.js
 const MongoClient = mongodb.MongoClient
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
@@ -13,8 +13,50 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: "Slim",
-        age: 46
+    // db.collection('users').insertOne({
+    //     name: "Slim",
+    //     age: 46
+    // }, (error, result) => {
+    //     if(error){
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.insertedId)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Johnny',
+    //         age: 33
+    //     },
+    //     {
+    //         name: 'Samantha',
+    //         age: 22
+    //     }
+    // ], (error, result) => {
+    //     if(error){
+    //         return console.log('Unable to insert docuents')
+    //     }
+
+    //     return console.log(result.insertedIds)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            task: 'Sleep',
+            completed: false
+        },{
+            task: 'Eat',
+            completed: true
+        },{
+            task: 'Clean the cat.',
+            completed: false
+        }
+    ], (error, result) => {
+        if(error){
+            return console.log(error)
+        }
+
+        return console.log(result.insertedIds)
     })
 })
