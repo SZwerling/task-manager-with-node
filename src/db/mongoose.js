b0720bbee7f1161d45a8514b1d28f7571a1df5a7
unknown <sazwerling@gmail.com>
2022-02-23 16:12:28 -0800

    data validation and sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,44 +1,64 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
 
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be a positive number.')
+            }
+        }
     }
 })
 
 const Task = mongoose.model('Task', {
     task: {
-        type: String
+        type: String,
     },
     completed: {
         type: Boolean
     }
 })
 
-const thing = new Task({
-    task: "Sleep",
-    completed: true
-})
-
-thing.save().then(() => {
-    console.log(thing)
-}).catch((error) => {
-    console.log('Error: ', error)
-})
-
-// const me = new User({
-//     name: "Slim",
-//     age: 46
+// const thing = new Task({
+//     task: "Sleep",
+//     completed: true
 // })
 
-// me.save().then(() => {
-//     console.log(me)
+// thing.save().then(() => {
+//     console.log(thing)
 // }).catch((error) => {
-//     console.log("error: ", error)
+//     console.log('Error: ', error)
 // })
+
+const me = new User({
+    name: '  Tom ',
+    email: 'someTHTHTingwrong@gmail.com  '
+})
+
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log("error: ", error)
+})
