6129611f97ef1f72c9a93d011f8c9ce42c9076db
unknown <sazwerling@gmail.com>
2022-02-28 12:33:24 -0800

    resource creation endpoints pt.2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,50 +1,11 @@
 //get ready for rest api
 //REpresentational State Transfer
 //Application Programming Interface
-const mongoose = require('mongoose');
-const validator = require('validator')
+const mongoose = require('mongoose')
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
 
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password cannot contain the word "password."')
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value){
-            if(value < 0){
-                throw new Error('Age must be a positive number.')
-            }
-        }
-    }
-})
 
 const Task = mongoose.model('Task', {
     task: {
@@ -59,26 +20,4 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const thing = new Task({
-    task: "Sleep",
-    completed: true
 
-})
-
-thing.save().then(() => {
-    console.log(thing)
-}).catch((error) => {
-    console.log('Error: ', error)
-})
-
-// const me = new User({
-//     name: '  Tom ',
-//     email: 'someTHTHTingwrong@gmail.com  ',
-//     password: 'zwerling123'
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log("error: ", error)
-// })
