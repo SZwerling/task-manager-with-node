6460d1595d38a18c26661cddba0d221292e528c7
unknown <sazwerling@gmail.com>
2022-03-08 17:14:44 -0600

    delete task by id

--- src/index.js
+++ src/index.js
@@ -135,7 +135,21 @@ app.patch('/tasks/:id', async(req,res) => {
         const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
 
         if(!task){
-            res.status(404).send()
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task){
+            return res.status(404).send()
         }
 
         res.send(task)
