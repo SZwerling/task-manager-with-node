5d9c0c57c3ecb19b70228b07b6d7ce142a7ebaad
unknown <sazwerling@gmail.com>
2022-03-14 19:47:01 -0500

    accept authentication tokens

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ const port = process.env.PORT || 3000
 //     }
 // })
 
-app.use((req, res, next) => {
-    res.status(503).send('Sorry. Server under mainentance.')
-})
+// app.use((req, res, next) => {
+//     res.status(503).send('Sorry. Server under mainentance.')
+// })
 
 app.use(express.json())
 app.use(userRouter)
