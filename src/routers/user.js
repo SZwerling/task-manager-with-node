61e9709f29c3a16c77f7bcefede4a11e8454f171
unknown <sazwerling@gmail.com>
2022-03-17 22:09:54 -0500

    logout auth tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -25,9 +25,32 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutall', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.get('/users/me', auth, async (req, res) => {
    res.send(req.user)
 })
+
 
 router.get('/users/:id', async (req,res) => {
     const _id = req.params.id
