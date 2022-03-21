f46afba1a697fd5b0e438b20f73c0c6d09fd4926
unknown <sazwerling@gmail.com>
2022-03-21 17:52:05 -0500

    user-task relationship

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require('./db/mongoose')
 
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const { db } = require('./models/user')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -31,6 +32,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+const main = async () => {
+    // const task = await Task.findById("6238f873ddfad1be9834fc5d")
+    // await task.populate('owner')
+    // console.log(task.owner);
+    const user = await User.findById("6238f734f0cb43852ae8c5f6")
+    await user.populate('tasks')
+    console.log(user.tasks)
+}
+main()
 
 
 
