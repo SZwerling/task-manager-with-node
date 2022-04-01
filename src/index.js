7a93be4c752fd8eaf93f84359f1bab2538635da4
unknown <sazwerling@gmail.com>
2022-04-01 10:35:27 -0500

    handle express error example

--- src/index.js
+++ src/index.js
@@ -24,6 +24,8 @@ const upload = multer({
 })
 app.post('/upload', upload.single('uploadGenericName'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 app.use(express.json())
